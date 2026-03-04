a7fa5ea15d3b68823148c6639f411b4fabe467e8
ITZ-SH1NZ <tejaskm2508@gmail.com>
2026-03-04 23:12:09 +0530

    responsive fix

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -5,7 +5,7 @@ export async function createClient() {
     const cookieStore = cookies()
 
     return createServerClient(
-        'https://skloop.jiobase.com',
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
         process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
         {
             cookies: {
