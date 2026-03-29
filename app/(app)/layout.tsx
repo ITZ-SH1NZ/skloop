7f758ad9593dc923a9d5c57366527cd9273b90d8
Wrath Lord <scenemwone75@gmail.com>
2026-03-29 07:06:16 +0530

    package fix

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -5,6 +5,8 @@ import { AppPreloader } from "@/components/providers/AppPreloader";
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 
+export const dynamic = "force-dynamic";
+
 export default async function AppLayout({
     children,
 }: {
